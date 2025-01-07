4f68418a9c225a0cb847fc56bac99d87458c00f8
Hyejin Lee <jin01251@naver.com>
2025-01-07 15:58:29 +0900

    큐

--- 카드2.py
+++ 카드2.py
@@ -1,13 +1,11 @@
-from collections import deque
-
 n = int(input())
-c = deque([])
+c = []
 
 for i in range(1, n+1):
     c.append(i)
 
 while len(c) > 1:
-    c.popleft()
-    c.append(c.popleft())
+    c.pop(0)
+    c.append(c.pop(0))
 
 print(c[0])
