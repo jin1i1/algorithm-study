5accd574ef4f38536960f31ede0ce9e75335ff9c
Hyejin Lee <jin01251@naver.com>
2025-01-08 11:05:13 +0900

    주석 추가

--- 카드2(큐).py
+++ 카드2(큐).py
@@ -4,8 +4,8 @@ c = []
 for i in range(1, n+1):
     c.append(i)
 
-while len(c) > 1:
+while len(c) > 1:       # 리스트의 길이가 1이 될 때까지 반복
     c.pop(0)
-    c.append(c.pop(0))
+    c.append(c.pop(0))  # 리스트의 첫번째 요소를 제거하고, 그 다음 요소를 리스트의 마지막에 추가
 
 print(c[0])
