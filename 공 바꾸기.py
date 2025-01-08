6af52a7d20c5d5f98a79f361befe3f9029b8e055
Hyejin Lee <jin01251@naver.com>
2025-01-08 11:00:53 +0900

    주석 정리

--- 공 바꾸기.py
+++ 공 바꾸기.py
@@ -1,15 +1,13 @@
-#공 바꾸기
+n, m = map(int, input().split())    # N (1 ≤ N ≤ 100)과 M (1 ≤ M ≤ 100)이 주어진다.
 
-n, m = map(int, input().split()) # N (1 ≤ N ≤ 100)과 M (1 ≤ M ≤ 100)이 주어진다.
-
-b = [i for i in range(1, n+1)] # 1번부터 N번까지 공이 차례대로 번호가 적힌 것
+b = [i for i in range(1, n+1)]      # 1번부터 N번까지 공이 차례대로 번호가 적힌 것
 t = 0
 
 for i in range(m):
     i,j = map(int, input().split()) # (1 ≤ i ≤ j ≤ N)
-    t = b[i-1] # 공 위치 교환
+    t = b[i-1]                      # 공 위치 교환
     b[i-1] = b[j-1]
     b[j-1] = t
 
 for i in range(n):
-    print(b[i], end=' ') # 공의 번호를 공백으로 구분해 출력
+    print(b[i], end=' ')            # 공의 번호를 공백으로 구분해 출력
