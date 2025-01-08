43b5489f7aab5db840384bf2fa3e4e9f6ea55e0c
Hyejin Lee <jin01251@naver.com>
2025-01-08 11:04:31 +0900

    주석 추가

--- 카드2(덱).py
+++ 카드2(덱).py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque   # 덱 사용
 
 n = int(input())
 c = deque([])
@@ -6,8 +6,8 @@ c = deque([])
 for i in range(1, n+1):
     c.append(i)
 
-while len(c) > 1:
+while len(c) > 1:               # 덱의 길이가 1이 될 때까지 반복
     c.popleft()
-    c.append(c.popleft())
+    c.append(c.popleft())       # 덱의 첫번째 요소를 제거하고, 그 다음 요소를 덱의 마지막에 추가
 
 print(c[0])
